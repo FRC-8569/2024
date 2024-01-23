c9784c9ed3d60e16c5fa73b88ab80d1adef6b4bd
yd-tw <yd960528@gmail.com>
2024-01-23 14:13:46 +0800

    rename var

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,35 +14,48 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-
-  private MotorControllerGroup m_right;
-  private MotorControllerGroup m_left;
-  private DifferentialDrive m_drive;
+  private CANSparkMax motor1;
+  private CANSparkMax motor2;
+  private CANSparkMax motor3;
+  private CANSparkMax motor4;
+  private CANSparkMax motor5;
+  private CANSparkMax motor6;
+  private CANSparkMax motor7;
+  private RelativeEncoder encoder5;
+  private RelativeEncoder encoder6;
+  private RelativeEncoder encoder7;
+  private MotorControllerGroup right;
+  private MotorControllerGroup left;
+  private DifferentialDrive drive;
   private Joystick joystick;
-  private CANSparkMax m_motor1;
-  private CANSparkMax m_motor2;
-  private CANSparkMax m_motor3;
-  private CANSparkMax m_motor4;
-
-  private CANSparkMax m_motor5;
-  private RelativeEncoder m_encoder5;
 
   @Override
   public void robotInit() {
-    m_motor1 = new CANSparkMax(1, MotorType.kBrushless);
-    m_motor2 = new CANSparkMax(2, MotorType.kBrushless);
-    m_motor3 = new CANSparkMax(3, MotorType.kBrushless);
-    m_motor4 = new CANSparkMax(4, MotorType.kBrushless);
-    m_motor5 = new CANSparkMax(5, MotorType.kBrushless);
+    motor1 = new CANSparkMax(1, MotorType.kBrushless);
+    motor2 = new CANSparkMax(2, MotorType.kBrushless);
+    motor3 = new CANSparkMax(3, MotorType.kBrushless);
+    motor4 = new CANSparkMax(4, MotorType.kBrushless);
+    motor5 = new CANSparkMax(5, MotorType.kBrushless);
+    motor6 = new CANSparkMax(5, MotorType.kBrushless);
+    motor7 = new CANSparkMax(5, MotorType.kBrushless);
 
-    m_motor5.restoreFactoryDefaults();
-    m_encoder5 = m_motor5.getEncoder();
-    m_encoder5.setPosition(0);
+    encoder5 = motor5.getEncoder();
+    encoder6 = motor6.getEncoder();
+    encoder7 = motor7.getEncoder();
 
-    m_left = new MotorControllerGroup(m_motor1, m_motor2);
-    m_right = new MotorControllerGroup(m_motor3, m_motor4);
+    motor5.restoreFactoryDefaults();
+    encoder5.setPosition(0);
 
-    m_drive = new DifferentialDrive(m_left, m_right);
+    motor6.restoreFactoryDefaults();
+    encoder6.setPosition(0);
+
+    motor7.restoreFactoryDefaults();
+    encoder7.setPosition(0);
+
+    left = new MotorControllerGroup(motor1, motor2);
+    right = new MotorControllerGroup(motor3, motor4);
+
+    drive = new DifferentialDrive(left, right);
     joystick = new Joystick(0);
   }
 
@@ -50,22 +63,31 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     double turnSpeed = 0.3 * joystick.getRawAxis(0);
     double driveSpeed = 0.5 * joystick.getRawAxis(5);
-    double position5 = m_encoder5.getPosition();
-    double speedMotor5 = m_encoder5.getVelocity();
+
+    double motorPos5 = encoder5.getPosition();
+    double motorSpd5 = encoder5.getVelocity();
+    double motorPos6 = encoder6.getPosition();
+    double motorSpd6 = encoder6.getVelocity();
+    double motorPos7 = encoder7.getPosition();
+    double motorSpd7 = encoder7.getVelocity();
 
     if (joystick.getRawButton(1)) {
-      m_motor5.set(0.3);
+      motor5.set(0.3);
     } else if (joystick.getRawButton(2)) {
-      m_motor5.set(-0.3);
+      motor5.set(-0.3);
     } else {
-      m_motor5.set(0);
+      motor5.set(0);
     }
 
-    m_drive.arcadeDrive(turnSpeed, driveSpeed);
+    drive.arcadeDrive(turnSpeed, driveSpeed);
 
     SmartDashboard.putNumber("turnSpeed", turnSpeed);
     SmartDashboard.putNumber("driveSpeed", driveSpeed);
-    SmartDashboard.putNumber("intakePos", position5);
-    SmartDashboard.putNumber("intakeSpd", speedMotor5);
+    SmartDashboard.putNumber("intakePos", motorPos5);
+    SmartDashboard.putNumber("intakeSpd", motorSpd5);
+    SmartDashboard.putNumber("motor6Pos", motorPos6);
+    SmartDashboard.putNumber("motor6Spd", motorSpd6);
+    SmartDashboard.putNumber("motor7Pos", motorPos7);
+    SmartDashboard.putNumber("motor7Spd", motorSpd7);
   }
 }
